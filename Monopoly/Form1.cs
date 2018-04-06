d6970189d752c65aa69adc5ec84cfdd892edab69
7ashish <hashish.mh@gmail.com>
2018-04-06 02:24:27 +0200

    Empty C# Project

---
+++ Monopoly/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monopoly
+{
+    public partial class Monopoly : Form
+    {
+        public Monopoly()
+        {
+            InitializeComponent();
+        }
+    }
+}
